da6b293d185ddd497ce990b6476202d654a3b0ea
Nada Hsayri <nada.hsayri@esprit.tn>
2026-02-05 20:50:31 +0100

    Add Campus Ideas header and footer layout

--- src/app/models/suggestion.ts
+++ src/app/models/suggestion.ts
@@ -1,2 +1,9 @@
-export interface Suggestion {
+export class Suggestion {
+  id!: number;
+  title!: string;
+  description!: string;
+  category!: string;
+  date!: Date;
+  status!: string;
+  nbLikes!: number;
 }
