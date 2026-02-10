6b1e13487baea8b3b9e0f055d9a9552c58934b26
Nada Hsayri <nada.hsayri@esprit.tn>
2026-02-10 19:11:02 +0100

    workshop3-create-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { ServerComponent } from './server/server.component';
 import { HeaderComponent } from './core/header/header.component';
 import { FooterComponent } from './core/footer/footer.component';
 import { ListSuggestionComponent } from './core/list-suggestion/list-suggestion.component';
+import { HomeComponent } from './core/home/home.component';
+import { NotfoundComponent } from './core/notfound/notfound.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,9 @@ import { ListSuggestionComponent } from './core/list-suggestion/list-suggestion.
     ServerComponent,
     HeaderComponent,
     FooterComponent,
-    ListSuggestionComponent
+    ListSuggestionComponent,
+    HomeComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
