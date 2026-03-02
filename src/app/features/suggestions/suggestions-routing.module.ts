e1187e88f2bed8a8d6eb1bf568dcd90c267afde4
Nada Hsayri <nada.hsayri@esprit.tn>
2026-03-02 03:01:05 +0100

    Ajout de l'attribut nbLikes dans Suggestion et Génération du composant SuggestionFormComponent

--- src/app/features/suggestions/suggestions-routing.module.ts
+++ src/app/features/suggestions/suggestions-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { SuggestionsComponent } from './suggestions.component';
+import { ListSuggestionComponent } from '../../core/list-suggestion/list-suggestion.component';
 import { SuggestionDetailsComponent } from './suggestion-details/suggestion-details.component';
 
 const routes: Routes = [
-  { path: '', component: SuggestionsComponent },
+  { path: '', component: ListSuggestionComponent },
   { path: ':id', component: SuggestionDetailsComponent }
 ];
 
--- src/app/features/suggestions/suggestions.module.ts
+++ src/app/features/suggestions/suggestions.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { SuggestionsRoutingModule } from './suggestions-routing.module';
 import { SuggestionsComponent } from './suggestions.component';
 import { SuggestionDetailsComponent } from './suggestion-details/suggestion-details.component';
+import { SuggestionFormComponent } from './suggestion-form/suggestion-form.component';
 
 
 @NgModule({
   declarations: [
     SuggestionsComponent,
-    SuggestionDetailsComponent
+    SuggestionDetailsComponent,
+    SuggestionFormComponent
   ],
   imports: [
     CommonModule,
