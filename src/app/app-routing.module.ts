3d0de0c4d2d8310e54fb08add1f4d6f16ce31b0d
Nada Hsayri <nada.hsayri@esprit.tn>
2026-03-02 03:02:59 +0100

    Ajout de la route pour SuggestionFormComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './core/home/home.component'; 
 import { ListSuggestionComponent } from './core/list-suggestion/list-suggestion.component';
 import { NotfoundComponent } from './core/notfound/notfound.component';
+import { SuggestionFormComponent } from './features/suggestions/suggestion-form/suggestion-form.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch:'full'},
@@ -10,6 +11,7 @@ const routes: Routes = [
   {path:'listSuggestion', component: ListSuggestionComponent},
   {path: 'suggestions',loadChildren: () =>import('./features/suggestions/suggestions.module').then(m => m.SuggestionsModule)},
   {path: 'users',loadChildren: () =>import('./features/users/users.module').then(m => m.UsersModule)},
+  { path: 'suggestions/form', component: SuggestionFormComponent },
   {path:'**', component:NotfoundComponent}
 ];
 
